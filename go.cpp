f3f9eec7c913c1ce0f362cfbe0ebffa49c44192e
Annya88888 <lenel88@yandex.ru>
2017-07-06 16:15:30 +0300

    Insert

---
+++ go.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Insert number" << std::endl;
+	int num; 
+	std::cin >> num;
+	std::cout << "I know that this is " << num << "!!!" << std::endl; 
+	return 0;
+}
